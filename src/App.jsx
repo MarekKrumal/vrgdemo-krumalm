7763fc103bbffed056547cc02b84e97c85923215
BARONYXZ <krumalmarek@gmail.com>
2025-01-15 09:59:11 +0100

    setup commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center justify-center pt-10">
+        Vrg demo
+      </h1>
+    </div>
+  );
+}
